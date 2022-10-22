83f5172f351aec019e674b88db9f8ddb0fadfa9d
kolya <kovshik@inbox.ru>
2022-10-22 17:08:30 +0300

    https://github.com/users/mrkovshik/projects/1/views/1

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/geziyor/geziyor"
 	"github.com/geziyor/geziyor/client"
 	"github.com/geziyor/geziyor/export"
-	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,19 +17,25 @@ func main() {
 }
 
 func quotesParse(g *geziyor.Geziyor, r *client.Response) {
-	r.HTMLDoc.Find("body").Each(func(i int, s *goquery.Selection) {
-
-		sel := (r.HTMLDoc.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(3)")).Text()
-		fmt.Println(sel + "gh")
-		for i := 0; i < 10; i++ {
-
-			g.Exports <- map[string]interface{}{
-				"Округ":        s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(3)").Text(),
-				"Район":        s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(4)").Text(),
-				"Длительность": s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(5)").Text(),
-				"Дата и время отключения": s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(6)").Text(),
-			}
+	r.HTMLDoc.Find("table#plansiz tbody tr").Each(func(i int, s *goquery.Selection) {
+		cols := s.Find("td").Map(func(_ int, s *goquery.Selection) string { return strings.TrimSpace(s.Text()) })
+		g.Exports <- map[string]interface{}{
+			"Округ":        cols[2],
+			"Район":        cols[3],
+			"Длительность": cols[4],
+			"Дата и время отключения": cols[5],
 		}
+		//sel := (r.HTMLDoc.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(3)")).Text()
+		//fmt.Println(sel + "gh")
+
+		//for i := 0; i < 10; i++ {
+		//	g.Exports <- map[string]interface{}{
+		//		"Округ":                   s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(3)").Text(),
+		//		"Район":                   s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(4)").Text(),
+		//		"Длительность":            s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(5)").Text(),
+		//		"Дата и время отключения": s.Find("#ContentPlaceHolder1_Repeater3_tr1_" + strconv.Itoa(i) + " > td:nth-child(6)").Text(),
+		//	}
+		//}
 	})
 	if href, ok := r.HTMLDoc.Find("li.next > a").Attr("href"); ok {
 		g.Get(r.JoinURL(href), quotesParse)
