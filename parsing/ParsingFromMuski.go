74c88b83f1ddf4a296788ff550164d30a56d454e
nikolay <kovshik@inbox.ru>
2023-01-27 17:39:43 +0300

    new parsing algoritm (no json)

--- parsing/ParsingFromMuski.go
+++ parsing/ParsingFromMuski.go
@@ -1,22 +1,21 @@
 package parsing
 
 import (
-	"github.com/PuerkitoBio/goquery"
-	"github.com/geziyor/geziyor"
-	"github.com/geziyor/geziyor/client"
-	"github.com/geziyor/geziyor/export"
+	"fmt"
+	"log"
 	"strconv"
+
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	// "github.com/geziyor/geziyor"
+	// "github.com/geziyor/geziyor/client"
+	// "github.com/geziyor/geziyor/export"
+	// "strconv"
+	// "strings"
 	"time"
 )
 
-func ParceFromMuski() {
-	geziyor.NewGeziyor(&geziyor.Options{
-		StartURLs: []string{"https://www.muski.gov.tr"},
-		ParseFunc: quotesParse,
-		Exporters: []export.Exporter{&export.JSON{}},
-	}).Start()
-}
 type Outage struct {
 	ID int
 	City string
@@ -25,25 +24,50 @@ type Outage struct {
 	Duration time.Duration
 }
 
+
 const oldTimeFormat = "02.01.2006 15:04"
+func ParceFromMuski() {
+rowSlice:=make([]Outage,0)
+	doc, err := goquery.NewDocument("https://www.muski.gov.tr")
+    if err != nil {
+        log.Fatal(err)
+    }
+    table := doc.Find("table#plansiz")
+    table.Find("tr").Each(func(i int, row *goquery.Selection) {
+		fmt.Println("row ",  i)
+		if i>2{
+rowSlice=append(rowSlice, Outage{})
+k:=i-3
+	 row.Find("td").Each(func(j int, cell *goquery.Selection) {
 
-func quotesParse(g *geziyor.Geziyor, r *client.Response) {
+		fmt.Println("cell ",  j, cell.Text())
+		switch {
+		case j==2:
+			rowSlice[k].City=cell.Text()
+		case j==3:
+			rowSlice[k].District=cell.Text()
+		case j==4:
+			convertedDur, err := strconv.ParseInt(strings.Trim(cell.Text(), " Saat"), 0, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rowSlice[k].Duration=time.Duration(convertedDur)
+		case j==5:
+			parsedTime:=strings.Trim(cell.Text(), " ")
+			rowSlice[k].StartDate,err=time.Parse(oldTimeFormat, parsedTime)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	r.HTMLDoc.Find("table#plansiz tbody tr").Each(func(i int, s *goquery.Selection) {
-		cols := s.Find("td").Map(func(_ int, s *goquery.Selection) string { return strings.TrimSpace(s.Text()) })
-		parsedTime := cols[5]
-		newtime, _ := time.Parse(oldTimeFormat, parsedTime)
-		convertedDur, _ := strconv.ParseInt(strings.Trim(cols[4], " Saat"), 0, 64)
-		newDuration := time.Duration(convertedDur)
-		g.Exports <- map[string]interface{}{
-			"City":              cols[2],
-			"District":          cols[3],
-			"Outage duration":   newDuration,
-			"Outage start date": newtime,
 		}
+		
 
-	})
-	if href, ok := r.HTMLDoc.Find("li.next > a").Attr("href"); ok {
-		g.Get(r.JoinURL(href), quotesParse)
-	}
+    })
 }
+        fmt.Println(rowSlice)
+    })
+
+}
+
+
+
