f6126b9e368b21f3457459c03beb4795d2941df2
nikolay <kovshik@inbox.ru>
2023-02-18 10:31:33 +0300

    started working on newlogger

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
+	"os"
 
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -10,6 +12,7 @@ import (
 	"github.com/mrkovshik/Fethiye-Outage-Bot/internal/database"
 	district "github.com/mrkovshik/Fethiye-Outage-Bot/internal/pkg/district/postgres"
 	"github.com/robfig/cron"
+	"go.uber.org/zap"
 
 	"github.com/mrkovshik/Fethiye-Outage-Bot/internal/pkg/outage/postgres"
 	"github.com/mrkovshik/Fethiye-Outage-Bot/internal/pkg/telegram"
@@ -19,6 +22,27 @@ import (
 var err error
 
 func main() {
+	// Open the configuration file
+	configFile, err := os.Open("logger_config.json")
+	if err != nil {
+		panic(err)
+	}
+	defer configFile.Close()
+
+	// Decode the configuration file into a zap.Config struct
+	var logCfg zap.Config
+	if err := json.NewDecoder(configFile).Decode(&logCfg); err != nil {
+		panic(err)
+	}
+
+    // Create a logger from the configuration
+    logger, err := logCfg.Build()
+    if err != nil {
+        panic(err)
+    }
+	defer logger.Sync()
+
+	//reading config file
 	cfg := config.GetConfig()
 	db := database.ConnectDB(cfg)
 	defer db.Close()
@@ -33,7 +57,7 @@ func main() {
 	c := cron.New()
 	store := postgres.NewOutageStore(db)
 	ds := district.NewDistrictStore(db)
-	err := c.AddFunc(cfg.SchedulerConfig.FetchPeriod, func() { store.FetchOutages(cfg) })
+	err = c.AddFunc(cfg.SchedulerConfig.FetchPeriod, func() { store.FetchOutages(cfg) })
 	c.Start()
 	if err != nil {
 		log.Fatalf("Sceduler error %v", err)
